60f20f8fea559f3fdd87917ef6c0b7da24ae8c4c
SietzeKoonstra <43609198+SietzeKoonstra@users.noreply.github.com>
2020-03-06 12:33:23 +0100

    update
    added ttn things

--- app.py
+++ app.py
@@ -4,13 +4,45 @@ from config import config
 import os
 from io import StringIO
 import ttn
+import base64
+
+message = "nog niks"
+# ------BEGIN MQTT TTN PART------ #
+
+app_id = "co2_sensor_stenden"
+access_key = "ttn-account-v2.J5ws5KGhK9jVP5p56HfG1VyLka8PecrVTtIsam6MpWA"
+
+def uplink_callback(msg, client):
+  print("Received uplink from ", msg.dev_id)
+  print(msg)
+  global message
+  data = base64.b64decode(msg.payload_raw).decode('ascii')
+  data = data.split(',')
+  humidity = data[1]
+  message = temperature = data[2]
+  print("deviceID: " + msg.dev_id)
+  print("humidity: " + humidity)
+  print("temperature: " + temperature)
+
+handler = ttn.HandlerClient(app_id, access_key)
+
+
+mqtt_client = handler.data()
+mqtt_client.set_uplink_callback(uplink_callback)
+mqtt_client.connect()
+
+# ------END MQTT TTN PART------ #
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Server is running!";
+def landing():
+    return "Server is running!"
 
+@app.route("/TTN")
+def hello():
+    global message
+    return message
 
 if __name__ == '__main__':
     app.run(debug=True)
